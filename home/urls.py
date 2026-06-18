f0a334a988768acac654de44b2188e85c571ea83
Amansingh086 <singhamanrajput10@gmail.com>
2026-06-18 16:49:00 +0530

    addedd profile page

--- home/urls.py
+++ home/urls.py
@@ -13,6 +13,8 @@ from .views import (
     vendor_responses,
     privacy_policy,
     term_condition,
+     profile,
+
 )
 
 urlpatterns = [
@@ -28,4 +30,5 @@ urlpatterns = [
     path('services/request_service/', request_service, name='request_service'),
     path('privacy-policy/', privacy_policy, name='privacy-policy'),
     path('term-condition/', term_condition, name='term_condition'),
+      path('profile/', profile, name='profile'),
 ]
